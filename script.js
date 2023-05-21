f97580bcf904cb900dfed591c0fbe9a7f4a17e33
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-05-21 07:06:11 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// Add two items
+const add = function(a , b) {
+    return a + b;
+}
+
+// Subtract two items
+const subtract = function(a , b) {
+    return a - b;
+}
+
+// Multiply two items
+const multiply = function(a , b) {
+    return a * b;
+}
+
+// Divide two items
+const divide = function(a , b) {
+    return a / b;
+}
+
