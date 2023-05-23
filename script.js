7cdd338d5d547610a69c666b44d05cb346e364c2
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-05-23 14:29:02 +0545

    Add OnClick Animation Properties

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
-const a = prompt("Enter The First Number >>> ",0) - 0;
+// const a = prompt("Enter The First Number >>> ",0) - 0;
 
-const b = prompt("Enter The Operator >>> ", '+');
+// const b = prompt("Enter The Operator >>> ", '+');
 
-const c = prompt("Enter The Second Number >>> ", 0) - 0;
-
-
+// const c = prompt("Enter The Second Number >>> ", 0) - 0;
 
 
-// Add two items
-const add = function(a , b) {
-    return (a + b);
-}
-
-// Subtract two items
-const subtract = function(a , b) {
-    return a - b;
-}
-
-// Multiply two items
-const multiply = function(a , b) {
-    return a * b;
-}
-
-// Divide two items
-const divide = function(a , b) {
-    return a / b;
-}
 
 
-if (b === '+') {
-    alert(add(a,c));
-} 
-else if (b === '-') {
-    alert(subtract(a,c));
-} 
-else if (b === '*') {
-    alert(multiply(a,c));
-} 
-else if (b === '/') {
-    alert(divide(a,c));
-} 
-else {
-    alert("Something Wrong! Please Try Again Later.");
-}
+// // Add two items
+// const add = function(a , b) {
+//     return (a + b);
+// }
+
+// // Subtract two items
+// const subtract = function(a , b) {
+//     return a - b;
+// }
+
+// // Multiply two items
+// const multiply = function(a , b) {
+//     return a * b;
+// }
+
+// // Divide two items
+// const divide = function(a , b) {
+//     return a / b;
+// }
+
+
+// if (b === '+') {
+//     alert(add(a,c));
+// } 
+// else if (b === '-') {
+//     alert(subtract(a,c));
+// } 
+// else if (b === '*') {
+//     alert(multiply(a,c));
+// } 
+// else if (b === '/') {
+//     alert(divide(a,c));
+// } 
+// else {
+//     alert("Something Wrong! Please Try Again Later.");
+// }
+
+
+
+
+
+
+
+// Define Variable for each element in DOM
+
+const clear = document.querySelector('.clear');
+const sign = document.querySelector('.sign');
+const percentage = document.querySelector('.percentage');
+const divide = document.querySelector('.divide');
+
+const multiply = document.querySelector('.multiply');
+const subtract = document.querySelector('.subtract');
+const add = document.querySelector('.add');
+const equal = document.querySelector('.equal-sign');
+
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+
+const decimal = document.querySelector('.decimal');
+
+
+
+
+
+// Write funtion for clearScreen
+
+const clearScreen = clear.addEventListener("click", (e) => {
+    
+});
